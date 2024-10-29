119f8f78fb10751c0d31885779f224a097a43d77
rejchev <hendlerq@yandex.ru>
2024-10-29 23:22:36 +0500

    updated: instance&type extraction on steam3 conversion

--- src/main/java/ru/rejchev/steamid/converters/steam3/StringSteam3Converter.java
+++ src/main/java/ru/rejchev/steamid/converters/steam3/StringSteam3Converter.java
@@ -2,13 +2,12 @@ package ru.rejchev.steamid.converters.steam3;
 
 import ru.rejchev.steamid.SteamID;
 import ru.rejchev.steamid.SteamIDAccountType;
+import ru.rejchev.steamid.SteamIDMaskType;
 import ru.rejchev.steamid.SteamIDUniverse;
 import ru.rejchev.steamid.converters.base.AStringSteamIDConverter;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringSteam3Converter extends AStringSteamIDConverter {
@@ -39,15 +38,35 @@ public class StringSteam3Converter extends AStringSteamIDConverter {
         if((steamID = super.convert(value)) == null)
             return null;
 
-        // TODO: ...
-        try { steamID
-                .setAccountId((Long.parseUnsignedLong(getMatcher().group(RequiredBinds[2]))))
-                .setInstance(1)
-                .setAccountUniverse(SteamIDUniverse.values()[Integer.parseUnsignedInt(getMatcher().group(RequiredBinds[1]))])
-                .setAccountType(SteamIDAccountType.of(Integer.parseUnsignedInt(getMatcher().group(OptionalBinds[0]))));
+        // TODO: testing
+        try {
 
-                if(getMatcher().group(OptionalBinds[1]) != null)
-                    steamID.setInstance(Long.parseUnsignedLong(getMatcher().group(OptionalBinds[0])));
+            String buff;
+            if((buff = getMatcher().group(RequiredBinds[0])) == null || buff.length() != 1)
+                return null;
+
+            char type = buff.charAt(0);
+
+            long instance = (type != 'g' && type != 'T' && type != 'c' && type != 'L') ? 0 : 1;
+
+            if((buff = getMatcher().group(OptionalBinds[0])) != null && !buff.isEmpty())
+                instance = Long.parseLong(buff);
+
+            if(type == 'c') {
+                instance |= ((SteamIDMaskType.AccountInstance.getMask() + 1) >> 1);
+                type = SteamIDAccountType.Chat.getChar();
+            }
+
+            if(type == 'L') {
+                instance |= ((SteamIDMaskType.AccountInstance.getMask() + 1) >> 2);
+                type = SteamIDAccountType.Chat.getChar();
+            }
+
+            steamID
+                .setAccountId((Long.parseUnsignedLong(getMatcher().group(RequiredBinds[2]))))
+                .setInstance(instance)
+                .setAccountUniverse(SteamIDUniverse.values()[Integer.parseUnsignedInt(getMatcher().group(RequiredBinds[1]))])
+                .setAccountType((type == SteamIDAccountType.InvalidCharacter) ? SteamIDAccountType.Invalid : SteamIDAccountType.of(type));
         }
         catch (NumberFormatException e) { return null; }
 
